85c834dd564d1eb7e9f52148f1af10578e0e0cff
sanchitha1336 <66420123+sanchitha1336@users.noreply.github.com>
2023-12-04 17:37:59 +0530

    Update SideBar.jsx

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './Card'
 import TextCard from './TextBgCard'
+import BarChat from './BarChat'
 
 const SideBar = () => {
     return (
@@ -222,20 +223,7 @@ const SideBar = () => {
                         <TextCard />
                     </div>
                     <div className="grid grid-cols-2 gap-4 mb-4">
-                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                            <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                                </svg>
-                            </p>
-                        </div>
-                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                            <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                                </svg>
-                            </p>
-                        </div>
+                     <BarChat />
                         <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
                             <p className="text-2xl text-gray-400 dark:text-gray-500">
                                 <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
